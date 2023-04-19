a43462f852600dc8c223e6396989552872ce8597
Robert E. Campbell Jr <robert.e.campbell.jr@gmail.com>
2023-04-19 14:21:46 -0600

    Header, footer, dark/light themes, and general setup completed.

---
+++ issue-3/lib.js
@@ -0,0 +1,33 @@
+function setFirstSection() {
+  const header = document.querySelector('header');
+  const body = document.querySelector('.first');
+  const headerHeight = header.offsetHeight;
+  body.style.marginTop = `${headerHeight}px`;
+}
+
+function setTheme(themeName) {
+  localStorage.setItem('theme', themeName);
+  document.documentElement.className = themeName;
+}
+
+function setThemePic(themeName) {
+  let toggleImg = document.getElementById('toggle-img');
+
+  if (themeName === 'dark') {
+    toggleImg.src = './pics/light.png';
+    toggleImg.alt = 'Light Mode';
+  } else if (themeName === 'light') {
+    toggleImg.src = './pics/dark.png';
+    toggleImg.alt = 'Dark Mode';
+  }
+}
+
+function toggleTheme() {
+  if (localStorage.getItem('theme') === 'dark') {
+      setTheme('light');
+      setThemePic('light');
+  } else {
+      setTheme('dark');
+      setThemePic('dark');
+  }
+}
