5c7d69418cf418e9168316b3f229875454d6c0ec
Robert E. Campbell Jr <robert.e.campbell.jr@gmail.com>
2023-04-27 00:23:49 -0600

    Form working, validating, and saving all new goals to localStorage. Table that will hold goals has been created and populated with initial goals.

--- issue-3/lib.js
+++ issue-3/lib.js
@@ -1,3 +1,6 @@
+const email = 'robecampbelljr@gmail.com';
+
+
 let setFirstSection = () => {
   const header = document.querySelector('header');
   const body = document.querySelector('.first');
@@ -42,4 +45,85 @@ let toggleTheme = () => {
     setTheme('dark');
     setThemePic('dark');
   }
+}
+
+let getGoal = () => {
+  const goal = document.getElementById('goal').value;
+  const description = document.getElementById('description').value;
+  const li1 = document.getElementById('li1').value;
+  const li2 = document.getElementById('li2').value;
+  const li3 = document.getElementById('li3').value;
+
+  return {
+    goa: goal,
+    des: description,
+    li1: li1,
+    li2: li2,
+    li3: li3
+  }
+}
+
+let saveGoal = () => {
+
+
+  if (validateForm()) {
+    let goals = [];
+
+    let savedGoals = localStorage.getItem('goal');
+
+    if (savedGoals !== null) {
+      goals = JSON.parse(savedGoals);
+    }
+
+    goals.push(getGoal());
+
+    localStorage.setItem('goal', JSON.stringify(goals));
+  }
+
+}
+
+let validateForm = () => {
+  const goalRegex = /^[a-zA-Z0-9\s]{5,50}$/;
+  const descriptionRegex = /^[a-zA-Z0-9\s.,!?]{5,100}$/;
+  const checklistRegex = /^[a-zA-Z0-9\s]{5,50}$/;
+
+  let goal = getGoal();
+
+  console.log((goal))
+
+  if (!goalRegex.test(goal.goa)) {
+    alert("Please enter a goal between 5 and 50 characters (letters and/or numbers only).");
+    return false;
+  }
+  if (!descriptionRegex.test(goal.des)) {
+    alert("Please enter a description between 5 and 100 characters (letters, numbers, spaces, and punctuation only).");
+    return false;
+  }
+
+    // if (!checklistRegex.test(goal.li1) || !checklistRegex.test(goal.li2) || !checklistRegex.test(goal.li3))
+
+  if (goal.li1 !== "") {
+    if (!checklistRegex.test(goal.li1)) {
+      alert("Please enter a checklist item between 5 and 50 characters (letters and/or numbers only).");
+      return false;
+    }
+  }
+
+  if (goal.li2 !== "") {
+    if (!checklistRegex.test(goal.li2)) {
+      alert("Please enter a checklist item between 5 and 50 characters (letters and/or numbers only).");
+      return false;
+    }
+  }
+
+  if (goal.li3 !== "") {
+    if (!checklistRegex.test(goal.li3)) {
+      alert("Please enter a checklist item between 5 and 50 characters (letters and/or numbers only).");
+      return false;
+    }
+  }
+
+
+  return true;
+
 }
